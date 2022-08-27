5be1a62ee82f380009955e1bd10ad6809126dc9a
lxxjxrn <almond26930@gmail.com>
2022-08-27 04:55:24 +0000

    Update more message

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  return res.send('<h1>123อันยองทึเม</h1>')
+  return res.send('<h1>สวัสดีมหาวิทยาลัยพะเยา</h1>')
 })
 
 const PORT = process.env.PORT || 3000
