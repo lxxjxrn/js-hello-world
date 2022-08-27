b81464c3555f25dc569c79e474e8c16184763225
lxxjxrn <almond26930@gmail.com>
2022-08-27 06:55:50 +0000

    treasure3

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  return res.send('<h1>lxxjern/h1>')
+  return res.send('<h1>lxxjern</h1>')
 })
 
 const PORT = process.env.PORT || 3000
