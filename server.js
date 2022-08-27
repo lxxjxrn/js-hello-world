956efb24d28ffb6964668f12121b82549ee0ad29
lxxjxrn <almond26930@gmail.com>
2022-08-27 07:08:18 +0000

    aa

--- server.js
+++ server.js
@@ -8,5 +8,5 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
-  console.info('Server is ready at', PORT)
+  console.info('Server is ready at3', PORT)
 })
